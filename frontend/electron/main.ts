a427a626f5bb61ede984983d3d1e8453d1289364
MannuVilasara <mannuvilasara@gmail.com>
2026-02-21 16:30:49 +0530

    Frontend config setup

---
+++ frontend/electron/main.ts
@@ -0,0 +1,54 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+// The built directory structure
+//
+// ├─┬─┬ dist
+// │ │ └── index.html
+// │ │
+// │ ├─┬ dist-electron
+// │ │ └── main.js
+// │
+process.env.DIST = path.join(__dirname, '../dist')
+process.env.VITE_PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public')
+
+let win: BrowserWindow | null
+// 🚧 Use ['ENV_NAME'] avoid vite:define plugin - SystemJS only
+const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        kiosk: false, // Full screen kiosk mode, set true in production
+        autoHideMenuBar: true,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    })
+
+    // Test active push message to Renderer-process.
+    win.webContents.on('did-finish-load', () => {
+        win?.webContents.send('main-process-message', (new Date).toLocaleString())
+    })
+
+    if (VITE_DEV_SERVER_URL) {
+        win.loadURL(VITE_DEV_SERVER_URL)
+    } else {
+        win.loadFile(path.join(process.env.DIST!, 'index.html'))
+    }
+}
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+        win = null
+    }
+})
+
+app.whenReady().then(createWindow)
---
+++ frontend/electron/preload.ts
@@ -0,0 +1,10 @@
+import { ipcRenderer } from 'electron'
+
+// --------- Expose some API to the Renderer process ---------
+declare global {
+  interface Window {
+	ipcRenderer: typeof ipcRenderer
+  }
+}
+
+window.ipcRenderer = ipcRenderer
